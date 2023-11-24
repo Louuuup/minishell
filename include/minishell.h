b0bed1ba2cb896314fc519e94e8439052c8eb91f
Maksim Kramer <mkramer@c2r4p4.42quebec.com>
2023-11-23 20:46:25 -0500

    update minishell.h before merge to master

--- include/minishell.h
+++ include/minishell.h
@@ -6,15 +6,19 @@
 /*   By: mkramer <mkramer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 17:27:09 by ycyr-roy          #+#    #+#             */
-/*   Updated: 2023/11/22 19:38:54 by mkramer          ###   ########.fr       */
+/*   Updated: 2023/11/23 20:45:49 by mkramer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include "../lib/libft/libft.h"
+# include "libft.h"
 # include <stdio.h>
+# include <readline.h>
+# include <history.h>
+# include <string.h>
+# include <fcntl.h>
 
 //==================VALUES===================//
 # define DEBUG_ON 0
@@ -22,9 +26,15 @@
 # define FALSE 0
 # define YES 1
 # define NO 0
+# define PROMPT_NAME "\033[35m\033[1mCoquillage Miniature \033[0m\033[1m→ \033[0m"
+# define EXPORT_PREFIX "declare -x "
 //==================ERRORS===================//
 # define ERR_MAIN "\033[31m\033[1mERROR > \033[0m"
 # define ERR_EXP "\033[37m\033[1mREASON\033[0m"
+# define ERR_FORK "Forked up!\n"
+# define ERR_DUP "Dup error\n"
+# define ERR_CD_ARGS "Too much arguments for CD\n"
+# define ERR_CD "Invalid name or path\n"
 //==================Structs===================//
 
 /**
@@ -33,11 +43,12 @@
 *@param	argv Array of cmd + all its args.
 *@param	next Next command.
 */
-// struct cmd;
+struct cmd;
 
 typedef struct cmd
 {
 	int			argc;
+	char		*cmd_path;
 	char		**args;
 	struct cmd	*next;
 }			t_cmd;
@@ -85,6 +96,9 @@ typedef struct s_data
 	int			code_exit;
 	int			cmdt_count;
 	int			count_pipes;
+	char		*user_prompt; //to free
+	char		**env; //to free
+	// int		exit_promt;	
 }	t_data;
 
 
@@ -125,7 +139,23 @@ t_data	*get_data(void);
 void	ft_error(char *str);
 void	my_error(char *str);
 //
-//==================[t_utils.c]===================//
+//==================[prompt.c]===================//
+void	prompt_create(t_data *data);
+//
+//==================[builtin.c]===================//
+void	build_pwd(int fd_out);
+//
+//==================[exec_utils.c]===================//
+void	ft_dup2(int fd, int std);
+//
+//==================[b_cd.c]===================//
+void	built_cd(char **args, int argc, int fd_out);
+//
+//==================[b_echo.c]===================//
+void	built_echo(char **args, int argc, int fd_out);
+//
+//==================[b_env.c]===================//
+void built_env(t_data *data);
 
 int		check_arguments(t_type_token type);
 

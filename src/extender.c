e041faf1fd158c8cdd475328d907b48ab7c11e4b
Maksim Kramer <mkramer@c2r4p4.42quebec.com>
2023-12-10 21:02:39 -0500

    add extender.c

---
+++ src/extender.c
@@ -0,0 +1,29 @@
+#include "../include/minishell.h"
+
+//what if we have "$" or "?" inside of a token? we need replace key with exception
+//the func extends tokens after checking variables
+
+int extender(t_data *pntr)
+{
+    // int exception;
+    // int stop;
+    // int i;
+
+    // stop = pntr->count_token;
+    // i = 0;
+    // while (i < stop)
+    // {
+    //     if (pntr->tokens[i].type == REDIRECT_MULTILINE)
+    //     {
+    //         i += 2;
+    //         while (pntr->tokens[i].no_space)
+    //             i++;
+    //     }
+    //     if ( && (pntr->tokens[i].type == DQUOTE || pntr->tokens[i].type == WORD))
+    //     {
+
+    //     }
+    //     i++;
+    // }
+    return (0);
+}

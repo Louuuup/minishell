c6fedfde71455c4575b75da640b4e28419cb65b4
Maksim Kramer <mkramer@c2r4p4.42quebec.com>
2023-12-12 18:36:39 -0500

    add if_has function

--- src/extender.c
+++ src/extender.c
@@ -1,29 +1,42 @@
 #include "../include/minishell.h"
 
+//auxiliary func, works like strchr
+
+int if_has(const char *n, char c)
+{
+	if (!n)
+		return (0);
+	while (*n)
+	{   if (*n == c)
+			return (1);
+	}
+	return (0);
+}
+
 //what if we have "$" or "?" inside of a token? we need replace key with exception
 //the func extends tokens after checking variables
 
 int extender(t_data *pntr)
 {
-    // int exception;
-    // int stop;
-    // int i;
+	int exception;
+	int stop;
+	int i;
 
-    // stop = pntr->count_token;
-    // i = 0;
-    // while (i < stop)
-    // {
-    //     if (pntr->tokens[i].type == REDIRECT_MULTILINE)
-    //     {
-    //         i += 2;
-    //         while (pntr->tokens[i].no_space)
-    //             i++;
-    //     }
-    //     if ( && (pntr->tokens[i].type == DQUOTE || pntr->tokens[i].type == WORD))
-    //     {
+	stop = pntr->count_token;
+	i = 0;
+	while (i < stop)
+	{
+		if (pntr->tokens[i].type == REDIRECT_MULTILINE)
+		{
+			i += 2;
+			while (pntr->tokens[i].no_space)
+				i++;
+		}
+		if (if_has(pntr->tokens[i].value, '$') && (pntr->tokens[i].type == DQUOTE || pntr->tokens[i].type == WORD))
+		{
 
-    //     }
-    //     i++;
-    // }
-    return (0);
+		}
+		i++;
+	}
+	return (0);
 }

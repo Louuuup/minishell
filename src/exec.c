b9a96f5a83b8fe1a04a588fb2bda1c8239c726df
yakary <yakarycr@gmail.com>
2023-12-15 13:55:17 -0500

    some token issues to fix

--- src/exec.c
+++ src/exec.c
@@ -1,4 +1,15 @@
-void	run_cmd(char **args, char *cmd_path, int in_fd, int out_fd)
-{
+#include "minishell.h"
 
+// void	run_cmd(char **args, char *cmd_path, int in_fd, int out_fd)
+// {
+
+// }
+
+void	exec_main(t_data *data)
+{
+	if (!data->cmdt)
+		return ;
+	if (data->cmdt->cmd)
+		printf("cmd1 = %s\n", data->cmdt->cmd);
+	
 }

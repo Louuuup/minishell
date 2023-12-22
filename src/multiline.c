6b20df529c2c33ffd93f5247b76c80ce15abd094
Maksim Kramer <mkramer@c2r4p4.42quebec.com>
2023-12-21 21:16:44 -0500

    create multiline.c

---
+++ src/multiline.c
@@ -0,0 +1,9 @@
+#include "minishell.h"
+
+//it makes a temp file, puts the heredoc into it,
+//then unlinks the file or sets it as the input
+
+int create_multiline(t_data *pntr, t_tab_cmd *s_tab_cmd, int i)
+{
+	
+}

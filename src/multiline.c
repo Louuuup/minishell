40f7dfe7e6ba5d6e956288a8700027e05101d7fd
Maksim Kramer <mkramer@c2r4p4.42quebec.com>
2023-12-22 20:12:37 -0500

    create signal.c

--- src/multiline.c
+++ src/multiline.c
@@ -15,6 +15,17 @@ char	name_create_multiline(int i)
 	return (file_name);
 }
 
+//it reads user input, stops on the delimiter, writes to a file descriptor
+
+int	input_to_file_descriptor(t_data *pntr, int fd, char *delimiter)
+{
+	static int		j;
+	char 	*line;
+
+	j = 0;
+	
+}
+
 //it makes a temp file, puts the heredoc into it,
 //then unlinks the file or sets it as the input
 
@@ -24,5 +35,13 @@ int	create_heredoc(t_data *pntr, t_tab_cmd *tab_cmd, int i)
 	int		status;
 	int		file_descriptor;
 
-	object = 0;
+	object = name_create_multiline(i);
+	if (!object)
+		return (error_out(pntr, 1), 1);
+	file_descriptor = open(object, O_CREAT | O_TRUNC | O_RDWR, 0666);
+	if (file_descriptor < 0)
+		return (free(object), error_out(pntr, 1));
+	status = 1;
+	
+	return (0);
 }

3a5103c2115e0a2ac9197c3d3ea7e721fb6c8138
Maksim Kramer <mkramer@c2r4p1.42quebec.com>
2023-12-28 19:12:23 -0500

    add recreate_file_descriptors

--- src/builtin.c
+++ src/builtin.c
@@ -1,15 +1,32 @@
 #include "minishell.h"
 
+//it's responsible for restoring file descriptors to their original states after executing a command
+
 void	recreate_file_descriptors(t_data *pntr, t_tab_cmd *cmd_tab, int i, int *pipe_fd)
 {
-	
-	write(1, "1", 1);
+	if (cmd_tab->out_fd != -1)
+		close(cmd_tab->out_fd);
+	if (pntr->cmdt_count - 1 != i)
+		pntr->fd_before = pipe_fd[0];
+	else
+		close(pipe_fd[0]);
+	dup2(pntr->first_stdout, STDOUT_FILENO);
+	dup2(pntr->first_stdin, STDIN_FILENO);
+	if (cmd_tab->in_fd != -1)
+		close(cmd_tab->in_fd);
+	if (cmd_tab->out_fd != -1)
+		close(cmd_tab->out_fd);
+	if (pntr->cmdt[i].last_multiline)
+	{
+		unlink(pntr->cmdt[i].last_multiline);
+		free(pntr->cmdt[i].last_multiline);
+	}
 }
 
 //it creates file descriptors for input/output in the builtin command
 //finishes a pipe file descriptor
 
-void    create_builtin_fd(t_tab_cmd *cmd_tab, int *pipe_fd)
+void	create_builtin_fd(t_tab_cmd *cmd_tab, int *pipe_fd)
 {
 	if (cmd_tab->out_fd != -1)
 		dup2(cmd_tab->out_fd, STDOUT_FILENO);
@@ -22,7 +39,11 @@ void    create_builtin_fd(t_tab_cmd *cmd_tab, int *pipe_fd)
 	close(pipe_fd[1]);
 }
 
-void    shoot_builtin(t_data *pntr, t_tab_cmd *cmd_tab, int i, int *pipe_fd)
+//function checks which built-in command is requested in the current cmd_table and calls the
+//corresponding function to handle the execution of that built-in command. After executing the
+//built-in command, it calls restore_fds to restore file descriptors to their original state
+
+void	shoot_builtin(t_data *pntr, t_tab_cmd *cmd_tab, int i, int *pipe_fd)
 {
 	create_builtin_fd(cmd_tab, pipe_fd);
 	if (ft_strcmp(cmd_tab->cmd, "exit") == 0)
@@ -41,6 +62,8 @@ void    shoot_builtin(t_data *pntr, t_tab_cmd *cmd_tab, int i, int *pipe_fd)
 		// built_echo(pntr, cmd_tab);
 	recreate_file_descriptors(pntr, cmd_tab, i, pipe_fd);
 }
+
+//check whether a given command is a built-in command
 
 int	if_builtin(t_tab_cmd *cmd_tab)
 {

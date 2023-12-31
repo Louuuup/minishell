dbf3be3edab3200af3f7a999c970da29a90c8f17
Maksim Kramer <mkramer@c2r4p1.42quebec.com>
2023-12-31 16:27:36 -0500

    add check_valid_execution ;

--- include/minishell.h
+++ include/minishell.h
@@ -17,6 +17,7 @@
 # include <sys/wait.h>
 # include <unistd.h>
 # include <errno.h>
+# include <dirent.h>
 
 //==================VALUES===================//
 # define DEBUG_ON 0
--- src/find_exec.c
+++ src/find_exec.c
@@ -1,24 +1,53 @@
 #include "minishell.h"
 
+//function checks if a specified command or file path is valid for execution. It checks whether the file exists, whether it is a directory, and whether it is executable. It prints appropriate error messages and sets the exit code accordingly. The function returns 0 for success and 1 for failure.
+
+int check_valid_execution(t_tab_cmd *tab_cmd, t_data *pntr)
+{
+	DIR *directory;
+
+	if (access(tab_cmd->cmd, 0) == 0)
+	{
+		directory = opendir(tab_cmd->cmd);
+		if (directory)
+		{
+			pntr->code_exit = 126;
+			ft_putstr_fd("minishell: that's a directory", 2);
+			closedir(directory);
+		}
+		else if (access(tab_cmd->cmd, X_OK) == 0)
+			return (0);
+		else
+		{
+			pntr->code_exit = 126;
+			ft_putstr_fd("minishell: No permission\n", 2);
+		}
+		return (1);
+	}
+	pntr->code_exit = 127;
+	ft_putstr_fd("minishell: No file / directory\n", 2);
+	return (1);
+}
+
 //is responsible for locating an executable binary in the system
 //and determining whether it is available for execution (127 - command not found)
 
 int find_exec(t_data *pntr, t_tab_cmd *cmd_tab)
 {
-    int i;
-    int result;
+	int i;
+	int result;
 
-    i = -1;
-    if (!cmd_tab->cmd)
-        return (1);
-    if (cmd_tab->cmd[0] == '\0')
-    {
-        ft_putstr_fd("minishell: command not found\n", 2);
-        pntr->code_exit = 127;
-        return (1);
-    }
-    if (ft_strchr(cmd_tab->cmd, '/') != 0 || cmd_tab->cmd[0] == '.')
-        return ("LATER!!!");
-    
-    return (1);
+	i = -1;
+	if (!cmd_tab->cmd)
+		return (1);
+	if (cmd_tab->cmd[0] == '\0')
+	{
+		ft_putstr_fd("minishell: command not found\n", 2);
+		pntr->code_exit = 127;
+		return (1);
+	}
+	if (cmd_tab->cmd[0] == '.' || ft_strchr(cmd_tab->cmd, '/') != 0)
+		return ("LATER!!!");
+	
+	return (1);
 }

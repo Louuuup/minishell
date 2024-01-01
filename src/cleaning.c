38d441e8d835722ad27aabb27ce3af6a179f8fd9
Maksim Kramer <mkramer@c2r4p1.42quebec.com>
2024-01-01 18:54:11 -0500

    ad pntr_cleaning

--- src/cleaning.c
+++ src/cleaning.c
@@ -1,5 +1,44 @@
 #include "minishell.h"
 
+//cleans memory taken by pntr->cmdt
+
+void	cmdt_cleaning(t_data *pntr)
+{
+
+}
+
+//the function is responsible for freeing memory allocated for various components of the t_data structure
+
+void	pntr_cleaning(t_data *pntr)
+{
+	int	i;
+
+	i = 0;
+	if (pntr->tokens)
+	{
+		while (0 < pntr->count_token)
+			free(pntr->tokens[--pntr->count_token].value);
+		free(pntr->tokens);
+		pntr->tokens = NULL;
+	}
+	cmdt_cleaning(pntr);
+	if (pntr->input)
+	{
+		free(pntr->input);
+		pntr->input = NULL;
+	}
+	if (pntr->path)
+	{
+		while (pntr->path[i])
+		{
+			free(pntr->path[i]);
+			i++;
+		}
+		free(pntr->path);
+		pntr->path = NULL;
+	}
+}
+
 //function for safely freeing the memory associated with an array of strings
 
 void	double_pntr_cleaning(char **pntr)
@@ -37,7 +76,7 @@ void	fd_cleaning(t_data *pntr, t_tab_cmd *tab_cmd, int i)
 void	total_clean(t_data *pntr)
 {
 	double_pntr_cleaning(pntr->env);
-	
+
 	close(pntr->first_stdin);
 	close(pntr->first_stdout);
 }

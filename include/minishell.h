98417e366697cb9cb8a5daf788decf7999335fb4
Felix Boivin <fboivin@c2r6p2.42quebec.com>
2024-01-29 15:20:25 -0500

    added cmd struct

--- include/minishell.h
+++ include/minishell.h
@@ -43,6 +43,38 @@ typedef struct s_data
 	t_memblock	*memblock; //head of allocated memory blocks
 }				t_data;
 
+//base for command struct can be modified if needed 
+
+typedef struct s_cmd
+{
+	char			**cmd;
+	char			*token;
+	int				index;
+	pid_t			pid;
+	int				fd_in;
+	int				fd_out;
+	int 			in_type;
+	int				out_type;
+	char 			*infile;
+	char			*outfile;
+	int				check;
+	struct s_cmd	*next;
+	bool			built_in;
+}			t_cmd;
+
+//flag fo redirection 
+
+enum e_redir_type
+{
+	REDIR_NONE, 	//stdin stdout
+	REDIR_INPUT,	// <
+	REDIR_OVERWRITE,// >
+	REDIR_APPEND,	// >>
+	REDIR_HEREDOC,	// <<
+	REDIR_PIPE		// |
+};
+
+
 
 //==================init.c===================//
 
